bf297910e64a084c6443acdd8fa0bf529f0e1507
Derek Graham <deejaygraham@gmail.com>
2025-07-18 13:20:22 +0100

    Fix regex and try import resource on windows

--- modules/interpreter.py
+++ modules/interpreter.py
@@ -1,6 +1,11 @@
 from hmac import new
 from multiprocessing import process
-import resource
+try:
+    import resource
+except ImportError:
+    # resource module is not available on Windows
+    resource = None
+    
 import modules.helpers as helpers
 import hcl2
 import click
@@ -289,11 +294,11 @@ def replace_var_values(
 def find_replace_values(varstring, module, tfdata):
     # Regex string matching to create lists of different variable markers found
     value = helpers.strip_var_curlies(str(varstring))
-    var_found_list = re.findall("var\.[A-Za-z0-9_\-]+", value)
-    data_found_list = re.findall("data\.[A-Za-z0-9_\-\.\[\]]+", value)
-    varobject_found_list = re.findall("var\.[A-Za-z0-9_\-]+\.[A-Za-z0-9_\-]+", value)
-    local_found_list = re.findall("local\.[A-Za-z0-9_\-\.\[\]]+", value)
-    modulevar_found_list = re.findall("module\.[A-Za-z0-9_\-\.\[\]]+", value)
+    var_found_list = re.findall(r"var\.[A-Za-z0-9_\-]+", value)
+    data_found_list = re.findall(r"data\.[A-Za-z0-9_\-\.\[\]]+", value)
+    varobject_found_list = re.findall(r"var\.[A-Za-z0-9_\-]+\.[A-Za-z0-9_\-]+", value)
+    local_found_list = re.findall(r"local\.[A-Za-z0-9_\-\.\[\]]+", value)
+    modulevar_found_list = re.findall(r"module\.[A-Za-z0-9_\-\.\[\]]+", value)
     # Replace found variable strings with variable values
     value = replace_data_values(data_found_list, value, tfdata)
     value = replace_module_vars(modulevar_found_list, value, module, tfdata)
